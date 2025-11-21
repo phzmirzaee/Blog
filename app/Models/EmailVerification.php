07e2cbe92decd534551889feadee2fa3b6c04bf1
Fatemeh Mirzaee <phz.mirzaee@gmail.com>
2025-11-21 23:41:06 +0330

    cartItem implemented

--- app/Models/EmailVerification.php
+++ app/Models/EmailVerification.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class EmailVerification extends Model
 {
@@ -10,4 +11,9 @@ class EmailVerification extends Model
     public $timestamps = false;
 
     protected $fillable = ['user_id', 'token', 'created_at'];
+
+    public function User():BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
